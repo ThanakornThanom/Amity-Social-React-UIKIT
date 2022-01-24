fffcceffb594f936c06fb9ad922eec74cb09aa8a
Au Thanakorn <thanakorn.t@amity.co>
2022-01-24 15:34:25 +0700

    make it better2

--- src/views/dashboard/app/live_stream.js
+++ src/views/dashboard/app/live_stream.js
@@ -11,8 +11,8 @@ const LiveStream =() =>{
         <div>
         <ReactFlvPlayer
           url = "http://live-stream.upstra-china.cc/5fca0b4aa6a6c18d7615886c/764bc4cb93d83fe1292c5851bf731270.flv?auth_key=1643022343-0-0-3b9cda5e686ea160d221852ac657625c"
-          heigh = "800px"
-          width = "800px"
+          heigh = "1000px"
+          width = "1000px"
           isMuted={true}
         />
       </div>
